e50b13ea8ce9c71af31c3ba4acfdc6b9efca50f7
czubi <czubi@DESKTOP-QQI3QN1>
2018-03-26 18:07:28 +0200

    Dodanie funkcji mierzących czas.

--- AVL2/AVL2/Source.cpp
+++ AVL2/AVL2/Source.cpp
@@ -1,38 +1,67 @@
 #include "AVLTree.h"
 #include <ctime>
 #include <cstdio>
+#include <Windows.h>
+
+#define RAND_MAX 0x7fff
+
 using namespace std;
 
+// funkcje potrzebne do mierzenia czasu tworzenia losowego drzewa, 
+// funkcje z biblioteki Windows.h
+// znalezione na stronie http://jaroslaw.mierzwa.staff.iiar.pwr.wroc.pl/pamsi/debug_and_time.pdf
+
+LARGE_INTEGER startTimer()
+{
+	LARGE_INTEGER start;
+	DWORD_PTR oldmask = SetThreadAffinityMask(GetCurrentThread(), 0);
+	QueryPerformanceCounter(&start);
+	SetThreadAffinityMask(GetCurrentThread(), oldmask);
+	return start;
+}
+LARGE_INTEGER endTimer()
+{
+	LARGE_INTEGER stop;
+	DWORD_PTR oldmask = SetThreadAffinityMask(GetCurrentThread(), 0);
+	QueryPerformanceCounter(&stop);
+	SetThreadAffinityMask(GetCurrentThread(), oldmask);
+	return stop;
+}
 void utworz_drzewo(avlTree &avl, int wielkosc) 
 {
 	srand(time(NULL)); 
 
 	for (int i = 0; i < wielkosc; i++) // wielkosc to zmienna okreslajaca ile elementow ma drzewo
 	{
-		avl.dodaj(avl.korzen, rand() % 1000); // losujemy z przedzialu od 0 do 256 i wstawiamy do drzewa
+		avl.dodaj(avl.korzen, rand() % 100000); // losujemy z przedzialu od 0 do 256 i wstawiamy do drzewa
 	}
 }
 
 int main()
 {
+	LARGE_INTEGER performanceCountStart, performanceCountEnd;
+	__int64 freq;
+	QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+	double tm;
+
 	int wybor, wartosc; // zmienna potrzebna do menu i zmienna do wrowadzania elementow 
 	avlTree avl; // utowrzenie obiektu typu avlTree o nazwie avl
 	while (1)
 	{
-		cout << "\n---------------------" << endl;
-		cout << "  Implementacja drzewa AVL   " << endl;
-		cout << "\n---------------------" << endl;
+		cout << "\n-------------------------------------------------" << endl;
+		cout << "            Implementacja drzewa AVL   " << endl;
+		cout << "\n-------------------------------------------------" << endl;
 		cout << "1. Dodaj element do drzewa" << endl;
 		cout << "2. Wyswietl zbalansowane drzewo" << endl;
-		cout << "3. Usuñ element o zadanym kluczu" << endl;
-		cout << "4. Wyœwietl wysokoœæ drzewa" << endl;
-		cout << "5. Wyœwietl korzen drzewa" << endl;
+		cout << "3. Usun element o zadanym kluczu" << endl;
+		cout << "4. Wyswietl wysokoœæ drzewa" << endl;
+		cout << "5. Wyswietl korzen drzewa" << endl;
 		cout << "6. Przejscie preorder" << endl;
 		cout << "7. Przejscie postorder" << endl;
 		cout << "8. Przejscie inorder" << endl;
 		cout << "9. Utworz losowe drzewo o wskazanej ilosci elementow" << endl;
 		cout << "10. Wyjdz " << endl;
-		cout << "\n-----------------------" << endl;
+		cout << "\n---------------------------------------------------" << endl;
 		cout << "Wybierz opcje: ";
 		cin >> wybor;
 		switch (wybor)
@@ -40,7 +69,11 @@ int main()
 		case 1:
 			cout << "Podaj wartosc do dodania : ";
 			cin >> wartosc;
+			performanceCountStart = startTimer();
 			avl.dodaj(avl.korzen, wartosc);
+			performanceCountEnd = endTimer();
+			tm = (double)(performanceCountEnd.QuadPart - performanceCountStart.QuadPart) / freq;
+			cout << endl << "czas tworzenia drzewa:" << tm << " s" << endl;
 			break;
 		case 2:
 			if (avl.korzen == nullptr)
@@ -80,7 +113,11 @@ int main()
 		case 9:
 			cout << "Wprowadz ilosc elementow: ";
 			cin >> wartosc;
+			performanceCountStart = startTimer();
 			utworz_drzewo(avl, wartosc);
+			performanceCountEnd = endTimer();
+			tm = (double)(performanceCountEnd.QuadPart - performanceCountStart.QuadPart) / freq;
+			cout << endl << "czas tworzenia drzewa:" << tm << " s" << endl;
 			break;
 		case 10:
 			exit(1);
